489d621af7dfc1c85fe5393ff102e8c0627524e9
GrichykhinSergey <90179867+GrichykhinSergey@users.noreply.github.com>
2021-09-10 12:58:22 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const text = document.querySelector('#text');
 const selected = document.querySelector('select')
 
-selected.addEventListener("click", () => text.value = document.querySelector(`[value=${selected.value}]`).textContent.trim());
+if (selected) {
+  selected.addEventListener("click", () => text.value = document.querySelector(`[value=${selected.value}]`).textContent.trim());
+}
 
 button.onclick = function() {
-  if (text.value.trim().length > 0) { 
+  if (text.value.trim().length > 0) {
+    text.style.borderColor = '#000000';
     document.querySelector(`[value=${selected.value}]`).textContent = text.value;
     text.value = document.querySelector(`[value=${selected.value}]`).textContent.trim();
   }
+  else {
+    text.style.borderColor = '#FF0000';
+  }
 }
-
